933d7bfeb3f99525852c727c76e04c24ec1c2831
okikokim808 <kikomanistheman15@gmail.com>
2018-08-30 10:34:21 -0700

    added models

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 //require express and other modules
 const express = require('express');
 const app = express();
-
 //parse incoming urlencoded data and populate req.body object
 const bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-  
+
+//password encryption extension
+const bcrypt = require('bcrypt');  
 //initialize database
 const db = require('./models');
+const jwt = require('jsonwebtoken')
 
 //serves static files from public folder
 app.use(express.static('public'));
 
 //html endpoints
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
