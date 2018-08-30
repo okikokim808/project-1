78e82d78792bfca8f4fdaeba4f1e5a66c8227332
Andrea  Piazza <andrea@Andreas-MacBook-Pro.local>
2018-08-29 17:58:01 -0700

    completed .ajax call

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,6 @@
-require('dotenv').config()
+// require('app.env').config()
 
-var meetupEndpoint = API-URL
+var meetupEndpoint = "https://api.meetup.com/2/concierge?&sign=true&photo-host=public&key=3b72576a30795b1d47673a2f3f2837"
 
 $(document).ready(function(){
     $.ajax({
@@ -12,3 +12,11 @@ $(document).ready(function(){
         }
     })
 })//end doc.ready
+
+function onSuccess(response){
+    var meetupJSONResponse = JSON.stringify(response)
+    
+    console.log('success ' + meetupJSONResponse)
+
+    
+}
