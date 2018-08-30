20077f63b5d94c5adfa9f9fb2f8a5d13a6abc5e8
Andrea  Piazza <andrea@andreas-mbp.attlocal.net>
2018-08-29 22:37:45 -0700

    fixed app.js file

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,7 @@
-require('app.env').config()
+// require('app.env').config()
 
-var meetupEndpoint = API-URL
+// var meetupEndpoint = API-URL
+var meetupEndpoint = "https://api.meetup.com/2/concierge?&sign=true&photo-host=public&key=3b72576a30795b1d47673a2f3f2837"
 
 $(document).ready(function(){
     $.ajax({
@@ -14,5 +15,9 @@ $(document).ready(function(){
 })//end doc.ready
 
 function onSuccess(response){
+    var meetupJSONResponse = JSON.stringify(response)
+    
+    console.log('success ' + meetupJSONResponse)
 
+    
 }
