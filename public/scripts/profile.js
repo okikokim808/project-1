0639beb38fb492411a46c15346969ec33e7a1a9c
Andrea  Piazza <andrea@Andreas-MacBook-Pro.local>
2018-09-01 16:04:21 -0700

    random function working, onSuccess call generating well formatted HTML

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,6 +1,6 @@
 //get location
 
-var locationEndpoint = "https://api.meetup.com/2/cities?offset=0&format=json&lon=-122.419998169&photo-host=public&page=500&radius=25.0&lat=37.7799987793&order=size&desc=false&sig_id=262151934&sig=2dcf462a53e32a4a6eb9a99b106d61daa927dc39"
+var locationEndpoint = "https://api.meetup.com/2/cities?&photo-host=public&query=13413&page=20&sign=true"
 
 function createSucc(user){    
     console.log(user.user._id)
@@ -56,8 +56,47 @@ $(document).ready(function(){
     })
 })//end doc.ready
 
+
+
+
 function onSuccess(response){
     var meetupJSONResponse = response.results;
-    $('#meetupList').append(meetupJSONResponse[0].description);
+    let maxLen = meetupJSONResponse.length;
+    //calculate random numbers
+        function randomNum(min,max,interval)
+        {
+            interval = 1;
+            let random = Math.floor(Math.random()*(max-0+interval)/interval);
+            return random*interval+min;
+        }
+            var num1 = randomNum(0,maxLen);
+            var num2 = randomNum(0,maxLen); 
+            var num3 = randomNum(0,maxLen);  
+             //rule out
+            if(num1 === num2 || num1 === num3 && num1 != maxLen){
+                num1++
+            }
+            else if(num2 === num3 && num2 != maxLen){
+                num2++ 
+            }
+            else if(num1 === num2 || num1 === num3 && num1=== maxLen){
+                num1--
+            }
+            else if(num2 === num3 && num2 === maxLen){
+                num2--
+            }
+
+            console.log(num1,num2,num3)
+
+    //generate HTML
+   console.log(num1,num2,num3)
+  
+    $("#list").append("<li>"+"<h2>Meetup 1" + " "+"</h2>"+meetupJSONResponse[num1].description+"<button name = `add` class = `addBtn` value =`add`>Add</button>" +"</li")
+
+    $("#list").append("<li>"+"<h2>Meetup 2" + " "+"</h2>"+meetupJSONResponse[num2].description+"<button name = `add` class = `addBtn` value =`add`>Add</button>" +"</li")
+
+    $("#list").append("<li>"+"<h2>Meetup 3" + " "+"</h2>"+meetupJSONResponse[num3].description+"<button name = `add` class = `addBtn` value =`add`>Add</button>" +"</li")
     console.log('success ', meetupJSONResponse)
 }
+
+//add button functionality
