a354fea1aa3597d45f4090f0c4cb14973141e2c9
Andrea  Piazza <andrea@Andreas-MacBook-Pro.local>
2018-09-01 11:14:16 -0700

    changed dataTypes in users.js

--- models/users.js
+++ models/users.js
@@ -2,18 +2,18 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
   email: { 
-    ype: String, 
+    type: String, 
     required: true, 
     unique: true, 
     match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/ },
   password: { type: String, required: true },
   username: {
-    formType: String,
+    type: String,
     required: true,
     unique: true
   },
-  interests:[{formType:String}],
-  meetupIDs: [{formType:Number}]
+  interests:[{type:String}],
+  meetupIDs: [{type:String}]
 })
 
 module.exports = mongoose.model('User', userSchema);
