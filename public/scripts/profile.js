349c624958c16e4401e760b70e7bea93be91ce1c
Andrea  Piazza <andrea@Andreas-MacBook-Pro.local>
2018-09-01 10:38:44 -0700

    API call working on profile page

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,5 +1,7 @@
+var meetupEndpoint = "https://api.meetup.com/2/concierge?&photo-host=public&key=3b72576a30795b1d47673a2f3f2837&callback=?&sign=true"
+
 $(document).ready(function(){
-    console.log('in app.js')
+    console.log('in profile.js')
     $.ajax({
         dataType: 'json',
         method: 'GET',
--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(function(req, res, next) {
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
-app.get('/profile', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/profile.html');
    });
 app.post('/verify', verifyToken, (req, res) => {
